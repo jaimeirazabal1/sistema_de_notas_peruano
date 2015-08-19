9e891ab400b4d33cb6faf46c918605af37af249a
jaimeirazabal1 <jaimeirazabal1@gmail.com>
2015-08-18 20:48:56 -0430

    listo modulo de alumnos crear y editar

--- default/app/controllers/alumno_controller.php
+++ default/app/controllers/alumno_controller.php
@@ -1,8 +1,41 @@
 <?php 
-
+Load::models("alumno");
 class AlumnoController extends AppController{
 
 	public function index(){
 		$this->titulo="Control de Alumnos";
+		$alumno = new Alumno();
+		$this->alumnos = $alumno->find();
 	}
- }?>
+	public function nuevo(){
+		if (Input::haspost("alumno")) {
+			$alumno = new Alumno(Input::post("alumno"));
+			$alumno->asignarIp();
+			if ($alumno->save()) {
+				Flash::valid("Alumno Registrado");
+			}else{
+				Flash::error("No se registro el alumno");
+			}
+		}
+		Router::redirect("alumno/");
+	}
+	public function editar($alumno_id){
+		$alumno = new Alumno();
+		
+		if (Input::haspost("alumno")) {
+			
+			$alumno_editando = new alumno(Input::post("alumno"));
+			$alumno_editando->asignarIp();
+			if ($alumno_editando->update()) {
+				Flash::valid("Registro editado");
+			}else{
+				Flash::error("El registro no pudo ser editado");
+			}
+			Router::redirect("alumno/");
+		}
+
+		$this->alumno = $alumno->find($alumno_id);
+	}
+ }
+
+ ?>
--- default/app/models/alumno.php
+++ default/app/models/alumno.php
@@ -1,6 +1,12 @@
 <?php 
 class Alumno extends ActiveRecord{
-	
+	protected function initialize(){
+		$this->validates_length_of("dni", 8,6);
+		$this->validates_length_of("celular", 18);
+   		$this->validates_uniqueness_of("dni","message: El dni ya se uso, por favor use otro");
+   		$this->validates_uniqueness_of("celular","message: El número celular ya se uso, por favor use otro");
+   
+	}
 }
 
  ?>
